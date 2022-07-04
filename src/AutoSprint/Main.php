3365272435172a5c20a5133bdf77426219914ac6
CraftCamp <93895629+CraftCamp1@users.noreply.github.com>
2022-07-04 14:08:39 +0800

    Create Main.php

---
+++ src/AutoSprint/Main.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AutoSprint;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
+use pocketmine\network\mcpe\protocol\types\PlayerAuthInputFlags;
+use pocketmine\utils\TextFormat as Fart;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+public static array $sprint = [];
+
+protected static self $main;
+
+    public function onEnable() : void {
+        self::$main = $this;
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+	public function onCommand(CommandSender $player, Command $command, string $label, array $args): bool {
+	    $name = $player->getName();
+        switch(strtolower($command->getName())) {
+            case "autosprint":
+            case "as":
+                if (!$player instanceof Player) {
+                $player->sendMessage(Fart::DARK_RED . "Use in-game!");
+                }else{
+                if(!in_array($name, self::$sprint)) {
+                    self::$sprint[] = $name;
+                    $player->sendMessage(Fart::GREEN . "You have enabled AutoSprint!");
+                }else{
+                    unset(self::$sprint[array_search($name, self::$sprint)]);
+                    $player->sendMessage(Fart::DARK_RED . "You have disabled AutoSprint!");
+                    }
+                }
+        }
+        return true;
+    }
+            
+    public function onPacketReceive(DataPacketReceiveEvent $event)
+    {
+        $player = $event->getOrigin()->getPlayer();
+        $packet = $event->getPacket();
+        if ($event->getPacket()->pid() === PlayerAuthInputPacket::NETWORK_ID) {
+            if (in_array($player->getName(), self::$sprint)) {
+                if($player->isSprinting() && $packet->hasFlag(PlayerAuthInputFlags::DOWN)){
+                            $origin->setSprinting(false);
+                }elseif(!$player->isSprinting() && $packet->hasFlag(PlayerAuthInputFlags::UP)){
+                            $player->setSprinting();
+                        }
+                }
+        }
+    }
+}
